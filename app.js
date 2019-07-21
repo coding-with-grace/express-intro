40da2da9dca60e8061e60f043b11f2903292adad
Grace Lee <igracelee1@gmail.com>
2019-07-20 23:51:57 -0400

    app listening notes

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ const express = require('express')
 const app = express()
 const items = require('./items')
 
-const PORT = 1337
 
 app.get('/', (req, res) => {
     res.send(items)
 })
 
 // app.get('/:id', (req, res) => {
-//     res.send(items[req.params.id])
-// })
-
-app.get('/:id', (req, res) => {
-    res.send(`I love ${req.params.id}!`)
-})
-
+    //     res.send(items[req.params.id])
+    // })
+    
+    app.get('/:id', (req, res) => {
+        res.send(`I love ${req.params.id}!`)
+    })
+    
+const PORT = 1337
+/*
+    when listening on our app, it's 
+    a good idea to have callback method to
+    show we're actually listening on a port
+*/
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
